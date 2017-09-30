c0b6eea68b1483fb523a903621524322e8c5c56d
Gregório Melo <gregorio@gregoriomelo.com>
2017-09-30 15:39:27 -0300

    Sets up maximum-awesome stuff

--- setup.sh
+++ setup.sh
@@ -3,27 +3,37 @@
 declare BASE_PATH=${1:-$HOME}
 declare DOTFILES_HOME=${BASE_PATH}/dotfiles
 
-# Check for required dependencies before continuing:
+mkdir -p $BASE_PATH
+
+echo "Checking for required dependencies before continuing..."
 hash git 2>/dev/null || { echo "Error: git is not installed. Please install git first."; exit 1;}
-
 hash curl 2>/dev/null || { echo "Error: curl is not installed. Please install curl first."; exit 1;}
-
 hash ruby 2>/dev/null || { echo "Error: ruby is not installed. Please install stow first."; exit 1;}
-
 hash brew 2>/dev/null || { echo "Installing Homebrew"; /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";}
 
+echo "Setting up vim Square's Maximum Awesome at $BASE_PATH/maximum-awesome"
+if [ ! -d "$BASE_PATH/maximum-awesome" ]; then
+  cd $BASE_PATH
+  git clone https://github.com/square/maximum-awesome.git
+fi
+cd $BASE_PATH/maximum-awesome
+rake
+
+echo "Getting my dotfiles and storing them at $DOTFILES_HOME"
 if [ ! -d "$DOTFILES_HOME" ]; then
-  git clone https://github.com/jonathanwiesel/dotfiles.git $DOTFILES_HOME
+  git clone https://github.com/gregoriomelo/dotfiles.git $DOTFILES_HOME
 else
   git -C $DOTFILES_HOME pull
 fi
 
-# Set up tmux plugin manager:
+echo "Setting up tmux plugin manager"
 mkdir -p ~/.tmux/plugins
 if [ ! -d ~/.tmux/plugins/tpm ]; then
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
+echo "Applying my personal set up"
+rm ~/.tmux.conf
 cd $DOTFILES_HOME
 brew tap homebrew/bundle
 brew bundle
