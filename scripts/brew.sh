57d900f8c5e6cbe550d750513b43694dd5ee5472
Gregório Melo <gregorio@gregoriomelo.com>
2018-01-08 21:13:57 -0200

    Checks whether XCode is installed

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -4,5 +4,13 @@ set -e
 
 echo "Installing apps with homebrew"
 cd $DOTFILES_HOME
+
+# installs xcode and accepts its license
+if [ ! -d /Applications/Xcode.app ]; then
+  mas install 497799835
+  sudo xcodebuild -license accept
+fi
+
+# and now installs the rest of the stuff
 brew tap homebrew/bundle
 brew bundle
